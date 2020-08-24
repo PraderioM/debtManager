87342d8c7e66ffef8a472a16e1ddd29db4cb4870
albertobolsi <66950864+albertobolsi@users.noreply.github.com>
2020-08-24 23:07:15 +0200

    Marco ha hecho magia.

--- backend/utils/remove_data_utils.py
+++ backend/utils/remove_data_utils.py
@@ -1,17 +1,26 @@
+import csv
+from glob import glob
+import os
+
 from backend.utils.get_data_utils import get_groups_data
-from backend.utils.get_path_utils import get_group_data_file_path
+from backend.utils.get_path_utils import get_group_data_file_path, get_group_data_dir
 
 
-def remove_group_data(name: str,
-                      mailgun_1: str,
-                      mailgun_2: str,
-                      line: int):
+def remove_group_data(id_: int):
+    # Get list of all groups and remove the one in specified position.
+    group_list = get_groups_data()
+    removed_group = group_list.pop(id_)
 
-    get_groups_data().remove(line)
+    # re-write all groups in database except the removed one.
+    with open(get_group_data_file_path(), 'w') as data_file:
+        writer = csv.writer(data_file)
+        for group in group_list:
+            writer.writerow(group.to_database())
 
+    # Remove all database files corresponding to removed group.
+    group_dir_path = get_group_data_dir(removed_group.name)
+    for file_path in glob(os.path.join(group_dir_path, '*')):
+        os.remove(file_path)
 
-
-
-
-
-
+    # Remove now empty directory.
+    os.rmdir(group_dir_path)
