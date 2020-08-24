7c1b8efc7e51ea527a82a7f8804b1aa027f42922
albertobolsi <66950864+albertobolsi@users.noreply.github.com>
2020-08-24 20:17:10 +0200

    "Flux" to "flow".

--- backend/utils/get_path_utils.py
+++ backend/utils/get_path_utils.py
@@ -1,10 +1,7 @@
-import datetime
 import os
-from glob import glob
-from typing import List, Optional
 
-from backend.constants import DATA_DIR_PATH, GROUP_DATA_FILE_NAME, MEMBERS_DATA_FILE_NAME, PERIODIC_FLUX_DATA_FILE_NAME, \
-    FLUX_DATA_DIR_NAME
+from backend.constants import DATA_DIR_PATH, GROUP_DATA_FILE_NAME, MEMBERS_DATA_FILE_NAME
+from backend.constants import PERIODIC_FLOW_DATA_FILE_NAME, FLOW_DATA_FILE_NAME
 
 
 def get_group_data_file_path() -> str:
@@ -19,20 +16,9 @@ def get_members_data_file_path(group_name: str) -> str:
     return os.path.join(get_group_data_dir(group_name), MEMBERS_DATA_FILE_NAME)
 
 
-def get_periodic_flux_data_file_path(group_name: str) -> str:
-    return os.path.join(get_group_data_dir(group_name), PERIODIC_FLUX_DATA_FILE_NAME)
+def get_periodic_flow_data_file_path(group_name: str) -> str:
+    return os.path.join(get_group_data_dir(group_name), PERIODIC_FLOW_DATA_FILE_NAME)
 
 
-def get_flux_data_dir(group_name: str) -> str:
-    return os.path.join(get_group_data_dir(group_name), FLUX_DATA_DIR_NAME)
-
-
-def get_all_flux_data_file_paths(group_name: str) -> List[str]:
-    return sorted(list(glob(os.path.join(get_flux_data_dir(group_name), '*'))))
-
-
-def get_flux_data_file_path(group_name: str, date: Optional[datetime.date] = None) -> str:
-    if date is None:
-        return get_all_flux_data_file_paths(group_name)[-1]
-    else:
-        return os.path.join(get_flux_data_dir(group_name), str(date) + '.csv')
+def get_flow_data_file_path(group_name: str) -> str:
+    return os.path.join(get_group_data_dir(group_name), FLOW_DATA_FILE_NAME)
