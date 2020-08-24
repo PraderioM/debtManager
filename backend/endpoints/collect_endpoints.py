ea6460202f5abb64a35d60350660b70023eb68ad
PraderioM <PraderioM@Gmail.com>
2020-08-24 23:14:24 +0200

    fixed import.

--- backend/endpoints/collect_endpoints.py
+++ backend/endpoints/collect_endpoints.py
@@ -1,11 +1,11 @@
 from aiohttp import web
 
-from .add_flow import add_debt
+from .add_flow import add_flow
 from .verify_connection import verify_connection
 from .get_token import get_token
 
 
 def collect_endpoints(app: web):
-    app.router.add_post('/add-debt', add_debt)
+    app.router.add_post('/add-debt', add_flow)
     app.router.add_post('/verify-connection', verify_connection)
     app.router.add_get('/get-token', get_token)
