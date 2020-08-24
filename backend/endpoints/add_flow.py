6f7315eb1d80bee5cb44db26ad45517b3507588b
albertobolsi <66950864+albertobolsi@users.noreply.github.com>
2020-08-24 20:15:42 +0200

    Elaborated add_flow function in add_flow.py.

--- backend/endpoints/add_flow.py
+++ backend/endpoints/add_flow.py
@@ -1,28 +1,39 @@
-from aiohttp import web
-from .verify_connection import verify_connection
-from .check_if_locked import check_if_locked
+from datetime import date
 import csv
+
+from aiohttp import web
+
+from backend.utils.check_if_locked import check_if_locked
+from backend.utils.get_data_utils import get_members_data
+from backend.utils.add_data_utils import add_members_data
+from backend.models.flow import Flow
 
 
 async def add_flow(request: web.Request) -> web.Response:
-    # Checking if member is locked.
     data = await request.post()
+
+    # Checking if member is locked.
     token = data["token"]
     if check_if_locked(token):
         return web.Response(
             status=200,
             body="Another person is using the app, sorry. You cannot modify stuff."
         )
-    issuer = data["issuer"]
-    receiver = data["receiver"]
-    amount = data["amount"]
-    concept = data["concept"]
-    date = data["date"]
 
-    # Adding
+    # Extracting inputs from frontend.
+    database_data = [data["issuer"], data["receiver"], data["amount"], data["concept"], str(date.today())]
+    flow = Flow.from_database(database_data, get_members_data(data['group_name']))
+
+    # Adding flow inputs from frontend into database.
+    add_members_data(flow, data['group_name'])
+
+    return web.Response(
+        status=201,
+        body="Flow added successfully."
+    )
 
 
 
 
-    # meter everything en csv
+
     # return a frontend, status 200
