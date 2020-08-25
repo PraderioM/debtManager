d4ee3eaf6915982cdd109b7155b303c2726fe236
PraderioM <PraderioM@Gmail.com>
2020-08-25 14:31:55 +0200

    Added endpoint.

--- backend/endpoints/get_periodic_flows_list.py
+++ backend/endpoints/get_periodic_flows_list.py
@@ -7,4 +7,7 @@ from backend.utils.get_data_utils import get_periodic_flow_data
 def get_periodic_flows_list(request: web.Request) -> web.Response:
     group_name = request.rel_url.query['group_name']
     periodic_flows_list = get_periodic_flow_data(group_name)
-    return web.Response(status=200, body=json.dumps([periodic_flow.to_frontend() for periodic_flow in periodic_flows_list]))
+    return web.Response(
+        status=200,
+        body=json.dumps([periodic_flow.to_frontend() for periodic_flow in periodic_flows_list])
+    )
